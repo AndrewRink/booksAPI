c2cb1ceadfaa15583cf0694d0840013cc7a0c7d5
Andrew <andrewcrink@gmail.com>
2022-12-20 20:50:11 -0500

    routes are working finally

---
+++ controllers/book.js
@@ -0,0 +1,69 @@
+const router = require('express').Router()
+const db = require('../models/index')
+
+//GET: get all the books
+router.get('/',(req,res) => {
+    db.Book.find()
+    .then ((books) => {
+        res.status(200).json(books);
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.status(400).json(err)
+    })
+})
+
+//GET: get a book by ID
+router.get('/:id', (req,res) => {
+    const { id } = req.params
+    db.Book.findById(id)
+    .then ((book) => {
+        res.status(200).json(book)
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.status(400).json(err)
+    })
+})
+
+//Post: create a book
+router.post('/', (req,res) => {
+    console.log(req.body)
+    db.Book.create(req.body)
+    .then ((book) => {
+        res.status(200).json(book)
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.status(400).json(err)
+    })
+})
+
+//Put: update a book
+router.put('/:id', (req,res) => {
+    const { id } = req.params
+    db.Book.findByIdAndUpdate(id, req.body)
+    .then ((book) => {
+        res.status(200).json(book)
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.status(400).json(err)
+    })
+})
+
+//Delete: Delete a Book
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    db.Book.findByIdAndDelete(id)
+    .then (() => {
+        res.status(200).json()
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.status(400).json(err)
+    })
+})
+
+
+module.exports = router
---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require ('express')
+const methodOverride = require('method-override')
+const mongoose = require ('mongoose')
+
+const bookRoutes = require('./controllers/book')
+
+const app = express()
+
+//MIDDLEWARE
+app.use(express.json())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+//routes
+app.use('/books', bookRoutes)
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello!<h1/>')
+})
+
+const PORT = process.env.PORT
+
+app.listen (PORT, console.log(`listening on port ${PORT}`))
